6bd8a418203f9156ac28726647817b1666642335
fabio henrique da silva <hantarodev@gmailcom>
2019-06-10 01:19:24 -0300

    Create Helper Class and added images resource

---
+++ app/src/main/java/hantaro/com/pokemonquiz/Helper.java
@@ -0,0 +1,26 @@
+package hantaro.com.pokemonquiz;
+
+public class Helper {
+
+
+    public static int [] getPokemonImage = {
+            R.drawable.pikachu,
+            R.drawable.goodra,
+            R.drawable.blatoise,
+            R.drawable.fletchling,
+            R.drawable.torracat,
+            R.drawable.bewear,
+            R.drawable.makuhita
+    };
+
+    public static String [][] getQuizText = {
+        {"picachu", "Bubblesaur", "charmander", "goomy"},
+            {"charizard", "goomy", "goodra", "sandile"},
+            {"blastoise", "charmander", "incineroar", "krokok"},
+            {"fletchling", "aipom", "growlithe", "torracat"},
+            {"torracat", "pikipek", "trumbeak", "pyukumuku"},
+            {"bewear", "spinda", "stufful", "lillipup"},
+            {"passimian", "drowzee", "spearow", "passimian"},
+
+    };
+}
